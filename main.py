6486fe4a43ef85693b37fef85a4863e8e7ab5c5e
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-05-09 11:03:33 +0200

    precition model added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ n = 1000
 
 data = {
     'tipo_de_cultivo': np.random.choice(['maiz', 'trigo', 'arroz', 'soja', 'naranjas','uva','lentejas', 'zanahoria'], n),
-    'ph': np.round(np.random.uniform(5.8, 6.8, n), 2),
+    'ph': np.round(np.random.uniform(4.5, 8.5, n), 2),
     "tipo_de_suelo": np.random.choice(["arcilloso", "arenoso", "limoso", "rocoso"], n),
     'humedad': np.round(np.random.uniform(0, 100, n), 2),
     'temperatura': np.round(np.random.uniform(0, 40, n)),
